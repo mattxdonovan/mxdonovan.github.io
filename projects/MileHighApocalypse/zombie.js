ebde1686f4c5b69b0bf1ea7b341f8be2a7dc5ce6
mattxdonovan <mattxd12@gmail.com>
2016-11-21 18:10:30 -0700

    updated about me page

--- projects/MileHighApocalypse/zombie.js
+++ projects/MileHighApocalypse/zombie.js
@@ -18,24 +18,24 @@ function rollDice() {
   // let uDie3 = Math.floor(Math.random() * 6) + 1
   let zDie1 = Math.floor(Math.random() * 6) + 1
   // let zDie2 = Math.floor(Math.random() * 6) + 1
+  $("#userDie").attr("src","images/O"+uDie1+".png");
   $("#zombieDie").attr("src","images/b"+zDie1+".png");
   // $("#zombieDie").attr("src","images/B"+zDie2+".png");
-  $("#userDie").attr("src","images/O"+uDie1+".png");
   // $("#youDie2").attr("src","images/O"+uDie2+".png");
   // $("#youDie3").attr("src","images/O"+uDie3+".png");
   $("#zombieDie").effect( "bounce" );
   $("#userDie").effect( "bounce" );
 
+
+  if(zDie1 === uDie1) {
+    status.innerHTML += "Tie... Roll Again"
+    $("#winner").attr("src","images/tie.png");
+    $("#winner").effect( "shake" );
+  } else if (zDie1 > uDie1) {
+    $("#winner").attr("src","images/zombie.gif");
+    $("#winner").effect( "puff" , 2000 );
+  }else{
+    $("#winner").attr("src",user.selectedHero.imageURL);
+    $("#winner").effect( "puff", 2000 );
+  }
 }
-//   if(zDie === uDie) {
-//     status.innerHTML += "Tie... Roll Again"
-//     $("#winner").attr("src","images/tie.png");
-//     $("#winner").effect( "shake" );
-//   } else if (zDie > uDie) {
-//     $("#winner").attr("src","images/zombie.gif");
-//     $("#winner").effect( "puff" , 2000 );
-//   }else{
-//     $("#winner").attr("src",user.selectedHero.imageURL);
-//     $("#winner").effect( "puff", 2000 );
-//   }
-// }
